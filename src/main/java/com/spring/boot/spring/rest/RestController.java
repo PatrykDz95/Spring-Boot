ef05c3db4aee646ed471ffbdb8a169ed87b6eb57
Patryk <patrykdziedzioch@gmail.com>
2019-05-10 20:48:01 +0200

    actuator security testing

--- src/main/java/com/spring/boot/spring/rest/RestController.java
+++ src/main/java/com/spring/boot/spring/rest/RestController.java
@@ -10,6 +10,16 @@ public class RestController {
     //expose '/' that returns
     @GetMapping("/")
     public String sayHello(){
-        return "Hello World " + LocalDateTime.now();
+        return "Hello World today is " + LocalDateTime.now();
+    }
+
+    @GetMapping("/work")
+    public String sayWork(){
+        return "Went for a run";
+    }
+
+    @GetMapping("/cos")
+    public String sayCos(){
+        return "Went for a run";
     }
 }
